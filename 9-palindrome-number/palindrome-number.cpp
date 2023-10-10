c08665694c599d742c8babee5fdccb4d545d21da
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-10-10 15:58:56 +0530

    Time: 11 ms (44.61%) | Memory: 6.9 MB (5.89%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,8 +4,7 @@ public:
         string s= to_string(x)+to_string(x);
         int n=s.length();
         int i=0;
-        for(i=0;i<n;i++)
-            if(s[i]!=s[n-i-1]) return false;
+        for(i=0;i<n;i++) if(s[i]!=s[n-i-1]) return false;
         return true;
     }
 };
