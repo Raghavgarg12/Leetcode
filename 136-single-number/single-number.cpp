b5497fb293983d367b3438f82e16b8be9dc1ae8a
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-10-10 15:49:41 +0530

    Time: 13 ms (70.22%) | Memory: 17.2 MB (45.63%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        //if (n==1) return nums[0];
-        //sort(nums.begin(),nums.end());
+        if (n==1) return nums[0];
+        sort(nums.begin(),nums.end());
         int i=0;
-        set<int> s;
-        for (int x : nums)
-            s.insert(x);
-        int sum1= 2*accumulate(s.begin(),s.end(),0);
-        int sum2= accumulate(nums.begin(),nums.end(),0);
-        return (sum1-sum2);
-        // for(i=0;i<n;i+=2)
-        // {
-        //     if(nums[i]!=nums[i+1]) return nums[i];
-        // }
-        // return 0;
+        // set<int> s;
+        // for (int x : nums)
+        //     s.insert(x);
+        // int sum1= 2*accumulate(s.begin(),s.end(),0);
+        // int sum2= accumulate(nums.begin(),nums.end(),0);
+        // return (sum1-sum2);
+        for(i=0;i<n;i+=2)
+        {
+            if(nums[i]!=nums[i+1]) return nums[i];
+        }
+        return 0;
     }
 };
