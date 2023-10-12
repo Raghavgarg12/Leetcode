a11b6b0cacbbe35d22a43409fe7b78721ea0b5b7
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-10-12 18:32:52 +0530

    Time: 17 ms (29.40%) | Memory: 20.1 MB (11.72%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,11 +1,3 @@
-// class Solution {
-// public:
-//     int majorityElement(vector<int>& nums) {
-//     int n=nums.size();
-//     sort(nums.begin(),nums.end());
-//     return nums[n/2];
-//     }
-// };
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
