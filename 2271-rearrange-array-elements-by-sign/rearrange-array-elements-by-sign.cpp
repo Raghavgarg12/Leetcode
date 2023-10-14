e2f78dc904f797a4dbb5dc71e731af8108f312f5
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-10-14 15:23:04 +0530

    Time: 480 ms (5.28%) | Memory: 124.2 MB (52.47%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,25 +2,15 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
     int n=nums.size(),i,j=0,k=1;
-    int arr[n];
-    //vector<int> pos(n);
+    vector<int> pos(n);
     // for(i=0;i<n;i++) if(nums[i]>0) pos.push_back(nums[i]); else neg.push_back(nums[i]);
     // nums.clear();
     // for(i=0;i<n/2;i++) nums.push_back(pos[i]), nums.push_back(neg[i]);
     for(i=0;i<n;i++){
-        if(nums[i]>0) {
-            arr[j]=nums[i];
-            //pos.insert(pos.begin()+j,nums[i]);
-            j+=2;
-        }
-        else {
-            arr[k]=nums[i];
-            //pos.insert(pos.begin()+k,nums[i]);
-            k+=2;
-        }
+        if(nums[i]>0) pos[j]=nums[i], j+=2;
+        else pos[k]=nums[i], k+=2;
         cout<<j<<k<<endl;
     }
-    vector<int> pos(arr,arr+n);
     return pos;   
     }
 };
