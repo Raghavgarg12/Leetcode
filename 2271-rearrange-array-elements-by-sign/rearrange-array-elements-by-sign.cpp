1ac404daf9dc48c835017869885e5436c7e4a2d8
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-10-14 15:23:38 +0530

    Time: 159 ms (64.44%) | Memory: 124.4 MB (44.02%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -3,13 +3,9 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
     int n=nums.size(),i,j=0,k=1;
     vector<int> pos(n);
-    // for(i=0;i<n;i++) if(nums[i]>0) pos.push_back(nums[i]); else neg.push_back(nums[i]);
-    // nums.clear();
-    // for(i=0;i<n/2;i++) nums.push_back(pos[i]), nums.push_back(neg[i]);
     for(i=0;i<n;i++){
         if(nums[i]>0) pos[j]=nums[i], j+=2;
         else pos[k]=nums[i], k+=2;
-        cout<<j<<k<<endl;
     }
     return pos;   
     }
