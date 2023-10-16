402bdc6f670c8aebe160e23525d84aa5d22f79b2
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-10-16 22:21:31 +0530

    Time: 6 ms (81.62%) | Memory: 6.7 MB (76.30%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -4,7 +4,6 @@ public:
         int n = s.length();
         int i,ans=0;
         vector<int> s2(n);
-        long int a=0;
         for(i=0;i<n;i++){
             if(s.at(i)=='I') s2[i]=1;
             else if(s.at(i)=='V') s2[i]=5;
@@ -13,15 +12,10 @@ public:
             else if(s.at(i)=='C') s2[i]=100;
             else if(s.at(i)=='D') s2[i]=500;
             else if(s.at(i)=='M') s2[i]=1000;
-            // cout<<s2[i];
         }
-        cout<<endl;
         for(i=n-1;i>=0;i--){
-            cout<<s2[i]<<" ";
             ans+=s2[i];
-            cout<<ans<<" ";
             if(i>0 && s2[i-1]<s2[i]) ans-=s2[i-1], i--;
-            cout<<ans<<endl;
         }
         return ans;
     }
