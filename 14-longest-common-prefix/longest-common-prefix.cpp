f745e50db17c3d02394272e208a662c9ab467104
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-10-22 14:46:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (40.82%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) {
-            return "";
-        }
-
-        // Find the shortest string in the array
-        std::string shortest = *std::min_element(strs.begin(), strs.end(),
-            [](const std::string& a, const std::string& b) {
-                return a.size() < b.size();
+    string longestCommonPrefix(vector<string>& v) {
+        string ans="";
+        sort(v.begin(),v.end());
+        int n=v.size();
+        string first=v[0],last=v[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
             }
-        );
-
-        for (int i = 0; i < shortest.size(); i++) {
-            char c = shortest[i];
-            for (const std::string& str : strs) {
-                if (str[i] != c) {
-                    return shortest.substr(0, i);
-                }
-            }
+            ans+=first[i];
         }
-
-        return shortest;
+        return ans;
     }
 };
