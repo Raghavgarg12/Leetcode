f914bd19112c472d98a69268056e9e33003e32ec
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-11-01 17:16:27 +0530

    Time: 11 ms (50.49%) | Memory: 16.4 MB (32.07%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        // sort(nums.begin(),nums.end());
+        // int n=nums.size();
+        // vector<int>ans;
+        // int count=1,s=n/3,i;
+        // for(i=1;i<n;i++){
+        //     if(nums[i]==nums[i-1]) count++;
+        //     else{
+        //         cout<<i<<endl;
+        //         if(count>s) ans.push_back(nums[i-1]);
+        //         count=1;
+        //     }
+        // }
+        // if(count>s) ans.push_back(nums[i-1]);
+        // return ans;
         int n=nums.size();
+        int el1,el2,count1=0,count2=0,i,s=n/3;
         vector<int>ans;
-        int count=1,s=n/3,i;
-        for(i=1;i<n;i++){
-            if(nums[i]==nums[i-1]) count++;
-            else{
-                cout<<i<<endl;
-                if(count>s) ans.push_back(nums[i-1]);
-                count=1;
-            }
+        for(i=0;i<n;i++){
+            if(count1==0 && el2!=nums[i]) count1=1, el1=nums[i];
+            else if(count2==0 && el1!=nums[i]) count2=1, el2=nums[i];
+            else if(el1==nums[i]) count1++;
+            else if(el2==nums[i]) count2++;
+            else count1--, count2--;
         }
-        if(count>s) ans.push_back(nums[i-1]);
+        count1=0,count2=0;
+        for(i=0;i<n;i++){
+            if(nums[i]==el1) count1++;
+            else if(nums[i]==el2) count2++;
+        }
+        if(count1>s) ans.push_back(el1);
+        if(count2>s) ans.push_back(el2);
         return ans;
     }
 };
