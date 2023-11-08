02b64479f2448ac07b33481f93a15170a9519e24
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-11-08 23:35:20 +0530

    Time: 328 ms (80.87%) | Memory: 111.4 MB (82.46%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,64 @@
+// class Solution {
+// public:
+//     int reversePairs(vector<int>& nums) {
+//         int n=nums.size();
+//         // sort(nums.begin(),nums.end());
+//         // unordered_map<int>map;
+//         int i,j,count=0;
+//         for(i=0;i<n;i++){
+//             for(j=n-1;j>i;j--){
+//                 if((long long)nums[i]>2*(long long)(nums[j])) count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+class Solution {
+private: 
+    void merge(vector<int>& nums, int low, int mid, int high, int& reversePairsCount){
+        int j = mid+1;
+        for(int i=low; i<=mid; i++){
+            while(j<=high && nums[i] > 2*(long long)nums[j]){
+                j++;
+            }
+            reversePairsCount += j-(mid+1);
+        }
+        int size = high-low+1;
+        vector<int> temp(size, 0);
+        int left = low, right = mid+1, k=0;
+        while(left<=mid && right<=high){
+            if(nums[left] < nums[right]){
+                temp[k++] = nums[left++];
+            }
+            else{
+                temp[k++] = nums[right++];
+            }
+        }
+        while(left<=mid){
+            temp[k++] = nums[left++]; 
+        }
+        while(right<=high){
+            temp[k++] = nums[right++]; 
+        }
+        int m=0;
+        for(int i=low; i<=high; i++){
+            nums[i] = temp[m++];
+        }
+    }
+
+    void mergeSort(vector<int>& nums, int low, int high, int& reversePairsCount){
+        if(low >= high){
+            return;
+        }
+        int mid = (low + high) >> 1;
+        mergeSort(nums, low, mid, reversePairsCount);
+        mergeSort(nums, mid+1, high, reversePairsCount);
+        merge(nums, low, mid, high, reversePairsCount);
+    }
+public:
+    int reversePairs(vector<int>& nums) {
+        int reversePairsCount = 0;
+        mergeSort(nums, 0, nums.size()-1, reversePairsCount);
+        return reversePairsCount;
+    }
+};
