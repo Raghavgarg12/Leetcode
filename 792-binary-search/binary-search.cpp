afdfd0c29557f059d89c1a688a1ee8f4e01eceb4
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-11-13 22:36:13 +0530

    Time: 24 ms (85.31%) | Memory: 27.8 MB (53.79%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -3,7 +3,10 @@ public:
     int search(vector<int>& nums, int target) {
         int n=nums.size();
         int i;
-        for(i=0;i<n;i++) if(nums[i]==target) return i;
+        if(target<nums[n/2]){
+            for(i=0;i<n/2;i++) if(nums[i]==target) return i;
+        }
+        else for(i=n/2;i<n;i++) if(nums[i]==target) return i;
         return -1;
     }
 };
