f6f82d0834639eae72bb384069f3ab037077d418
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-11-20 11:03:50 +0530

    Time: 3 ms (64.84%) | Memory: 11.4 MB (67.61%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,8 +1,28 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int n=nums.size();
+//         for(int i=0;i<n;i++) if(nums[i]==target) return i;
+//         return -1;
+//     }
+// };
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0;i<n;i++) if(nums[i]==target) return i;
+    int search(std::vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            else {
+                if (nums[mid] < target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
         return -1;
     }
 };
