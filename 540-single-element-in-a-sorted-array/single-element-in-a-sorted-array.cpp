5d88bf5c63e48b03505d2d6d4e7997a39934e0b1
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-11-21 21:12:03 +0530

    Time: 16 ms (80.76%) | Memory: 22.7 MB (53.45%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+    int result = 0;
+    for (int num : nums) result ^= num; // XOR operation
+    return result;
+    }
+};
