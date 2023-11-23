d9ea7a9e23bcce680cd5a5e0d69a238134597696
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-11-23 16:02:33 +0530

    Time: 18 ms (74.31%) | Memory: 22.9 MB (12.39%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,8 +1,51 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-    int result = 0;
-    for (int num : nums) result ^= num; // XOR operation
-    return result;
+    int n=nums.size();
+    int l=0,r=n-1,mid;
+    if(n<2) return nums[0];
+    while(l<=r)
+    {
+        mid=(l+r)/2;
+        cout<<mid<<endl;
+        if(mid>0 && mid<n-1)
+        {
+            if(nums[mid]==nums[mid-1])  //12241
+            {
+                if(mid%2==0)
+                {
+                    cout<<'1';
+                    r=mid-1;
+                    continue;
+                }
+                else
+                {
+                    cout<<'2';
+                    l=mid+1;
+                    continue;
+                }
+            }
+            else if(nums[mid]==nums[mid+1])
+            {
+                if(mid%2==0)
+                {
+                    cout<<'3';
+                    l=mid+1;
+                    continue;
+                }
+                else
+                {
+                    cout<<'4';
+                    r=mid-1;
+                    continue;
+                }
+            }
+            else return nums[mid];   
+        }
+        else return nums[mid];   
+    }
+    // int result = 0;
+    // for (int num : nums) result ^= num; // XOR operation
+    return nums[mid];
     }
 };
