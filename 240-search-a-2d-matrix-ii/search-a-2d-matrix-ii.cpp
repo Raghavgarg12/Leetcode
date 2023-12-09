5e516fb44e171f0b1299863fca5e5493a154b4d8
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-12-09 19:15:31 +0530

    Time: 85 ms (72.08%) | Memory: 15.1 MB (97.15%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,32 +1,32 @@
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        vector<int>col;
-        vector<int> row;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int i,j;
-        int rmin=INT_MAX,rmax=INT_MIN,cmin=INT_MAX,cmax=INT_MIN;
-        for(i=0;i<n;i++)
-        {
-            if(matrix[i][0]<=target && target<=matrix[i][m-1]) row.push_back(i);
-        }
-        for(i=0;i<m;i++)
-        {
-            if(matrix[0][i]<=target && target<=matrix[n-1][i]) col.push_back(i);
-        }
-        if(row.size()==0 || col.size()==0) return false;
-        for(auto i:row){
-            rmin=min(i,rmin);
-            rmax=max(rmax,i);
-        }      
-        for(auto i:col){
-            cmin=min(cmin,i);
-            cmax=max(cmax,i);
-        }
-        cout<<cmax<<endl;
-        row.clear();
-        col.clear();
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         vector<int>col;
+//         vector<int> row;
+//         int n=matrix.size();
+//         int m=matrix[0].size();
+//         int i,j;
+//         int rmin=INT_MAX,rmax=INT_MIN,cmin=INT_MAX,cmax=INT_MIN;
+//         for(i=0;i<n;i++)
+//         {
+//             if(matrix[i][0]<=target && target<=matrix[i][m-1]) row.push_back(i);
+//         }
+//         for(i=0;i<m;i++)
+//         {
+//             if(matrix[0][i]<=target && target<=matrix[n-1][i]) col.push_back(i);
+//         }
+//         if(row.size()==0 || col.size()==0) return false;
+//         for(auto i:row){
+//             rmin=min(i,rmin);
+//             rmax=max(rmax,i);
+//         }      
+//         for(auto i:col){
+//             cmin=min(cmin,i);
+//             cmax=max(cmax,i);
+//         }
+//         cout<<cmax<<endl;
+//         row.clear();
+//         col.clear();
         // for(i=rmin;i<=rmax;i++){
         //     if(matrix[i][cmin]<=target && target<=matrix[i][cmax]) row.push_back(i);
         // }
@@ -41,11 +41,25 @@ public:
         //     cmin=min(cmin,i);
         //     cmax=max(cmax,i);
         // }
-        for(i=rmin;i<=rmax;i++){
-            for(j=cmin;j<=cmax;j++){
-                if(matrix[i][j]==target) return true;
-            }
-        }
-        return false;
+//         for(i=rmin;i<=rmax;i++){
+//             for(j=cmin;j<=cmax;j++){
+//                 if(matrix[i][j]==target) return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix[0].size(),n=matrix.size();
+        int i=0,j=m-1;
+       while(i<n and j>=0)
+       {
+           if(matrix[i][j]==target) return true;
+           else if(matrix[i][j]<target) i++;
+           else j--;
+       }
+       return false;
     }
 };
