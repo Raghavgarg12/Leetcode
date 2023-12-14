ed8df95d627d8e07fe67a754fcdf6901d6a14ec9
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-12-14 22:39:31 +0530

    Time: 53 ms (5.10%) | Memory: 7.1 MB (41.02%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n =s.length();
+        cout<<n;
+        int i,l=0,x=0;
+        for(i=0;i<n;i++){
+            cout<<i;
+            if(s.at(i)=='(') l++;
+            else if (s.at(i)==')') l--;
+            if(l==0) {
+                s.erase(s.begin()+x);
+                s.erase(s.begin()+i-1);
+                i=i-2;
+                x=i+1;
+                n=s.length();
+            }
+        }
+        return s;
+    }
+};
