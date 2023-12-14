a9f63e3d132d226152dcd7b5c712ca24b64400f1
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2023-12-14 22:43:25 +0530

    Time: 4 ms (52.11%) | Memory: 7.1 MB (71.71%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,19 +1,31 @@
+// class Solution {
+// public:
+//     string removeOuterParentheses(string s) {
+//         int n =s.length();
+//         int i,l=0,x=0;
+//         for(i=0;i<n;i++){
+//             if(s.at(i)=='(') l++;
+//             else if (s.at(i)==')') l--;
+//             if(l==0) {
+//                 s.erase(s.begin()+x);
+//                 s.erase(s.begin()+i-1);
+//                 i=i-2;
+//                 x=i+1;
+//                 n=s.length();
+//             }
+//         }
+//         return s;
+//     }
+// };
 class Solution {
 public:
-    string removeOuterParentheses(string s) {
-        int n =s.length();
-        int i,l=0,x=0;
-        for(i=0;i<n;i++){
-            if(s.at(i)=='(') l++;
-            else if (s.at(i)==')') l--;
-            if(l==0) {
-                s.erase(s.begin()+x);
-                s.erase(s.begin()+i-1);
-                i=i-2;
-                x=i+1;
-                n=s.length();
-            }
+    string removeOuterParentheses(string S) {
+        string res;
+        int opened = 0;
+        for (char c : S) {
+            if (c == '(' && opened++ > 0) res += c;
+            if (c == ')' && opened-- > 1) res += c;
         }
-        return s;
+        return res;
     }
 };
