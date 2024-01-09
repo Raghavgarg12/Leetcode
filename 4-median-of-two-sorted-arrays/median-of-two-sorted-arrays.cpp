3dd0d6f8252255835b3f6c9ce9e2274e00c5eb26
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-01-09 23:48:57 +0530

    Time: 11 ms (99.27%) | Memory: 89.6 MB (70.01%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -0,0 +1,57 @@
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         int n=nums1.size();
+//         int m=nums2.size();
+//         int l1=0,r1=n-1,l2=0,r2=m-1,mid;
+//         mid=ceil((double)(n+m)/2);
+//         // cout<<mid;
+//         while(l1+l2+1<mid){
+//             if(nums1[l1]<nums2[l2]){
+//                 if(l1<n-1) l1++;
+//                 else l2++;
+//             }
+//             else{
+//                 if(l2<m-1) l2++;
+//                 else l1++;
+//             }
+//         }
+//         cout<<l1<<l2;
+//         if((n+m)%2==0) return (double)(nums1[l1]+nums2[l2])/2;
+//         else if(nums1[l1]<nums2[l2]) return nums2[l2];
+//         else return nums1[l1];
+
+//         return 0;
+//     }
+// };
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int n = n1 + n2;
+        int left = (n1 + n2 + 1) / 2;
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 1) return max(l1, l2);
+                else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+            }
+            else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
+        }
+        return 0;
+    }
+};
