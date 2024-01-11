e7742b4185bcab707ce86409336968e94698bf58
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-01-11 18:15:43 +0530

    Time: 3 ms (87.19%) | Memory: 46.8 MB (9.86%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -22,16 +22,10 @@ public:
                     continue;
                 }
                 ans=ans+" "+s.substr(i+1,k-i);
-                // cout<<s.substr(i+1,k-i)<<endl;
                 k=i-1;
-                // cout<<ans<<endl;
             }
-
         }
         ans=ans+" "+s.substr(0,k+1);
-    // s.erase(remove_if(s.begin(), s.end(), isspace), s.end());
-    // s.erase(std::remove_if(s.begin(), s.end(), ::isspace), s.end());
-    cout<<ans;
     ans.erase(0,1);
     return ans;
     }
