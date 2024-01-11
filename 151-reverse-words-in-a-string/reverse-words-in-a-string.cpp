ab3bffca27b136d9259f062d43267fee201619ac
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-01-12 01:17:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (40.10%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,29 +1,48 @@
-using namespace std;
+// using namespace std;
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         while(s.at(0)==' ') s.erase(0,1);
+//         int n = s.length(),i,k;
+//         string ans;
+//         while(s.at(n-1)==' ') {
+//             s.erase(n-1,n);
+//             n = s.length();
+//         }
+//         cout<<s;
+//         n=s.length();
+//         k=n-1;
+//         cout<<k<<endl;
+//         for(i=n-1;i>=0;i--){
+//             if(s.at(i)==' '){
+//                 if(s.at(i+1)==' ') {
+//                     k--;
+//                     continue;
+//                 }
+//                 ans=ans+s.substr(i+1,k-i)+" ";
+//                 k=i-1;
+//             }
+//         }
+//         ans=ans+s.substr(0,k+1);
+//     return ans;
+//     }
+// };
 class Solution {
 public:
     string reverseWords(string s) {
-        while(s.at(0)==' ') s.erase(0,1);
-        int n = s.length(),i,k;
-        string ans;
-        while(s.at(n-1)==' ') {
-            s.erase(n-1,n);
-            n = s.length();
+        vector<string>ans;
+        stringstream ss(s);
+        string token;
+        while(ss>>token){
+            ans.push_back(token);
         }
-        cout<<s;
-        n=s.length();
-        k=n-1;
-        cout<<k<<endl;
-        for(i=n-1;i>=0;i--){
-            if(s.at(i)==' '){
-                if(s.at(i+1)==' ') {
-                    k--;
-                    continue;
-                }
-                ans=ans+s.substr(i+1,k-i)+" ";
-                k=i-1;
-            }
+        reverse(ans.begin(),ans.end());
+        token="";
+        for(auto x:ans){
+            token +=x;
+            token+=" ";
         }
-        ans=ans+s.substr(0,k+1);
-    return ans;
+        token.pop_back();
+        return token;
     }
 };
