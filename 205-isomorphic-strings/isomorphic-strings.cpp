16c345b26f383dff9498bd04f5f7d486caa01334
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-01-16 00:04:27 +0530

    Time: 5 ms (68.27%) | Memory: 7.2 MB (91.25%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,12 +1,34 @@
+// class Solution {
+// public:
+//     bool isIsomorphic(string s, string t) {
+//          unordered_map<char, char> mp, mp2;
+//         for (int i=0; i<s.length(); ++i) {
+//             if (mp[s[i]] && mp[s[i]]!=t[i]) return false;
+//             if (mp2[t[i]] && mp2[t[i]]!=s[i]) return false;
+//             mp[s[i]]=t[i];
+//             mp2[t[i]]=s[i];
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-         unordered_map<char, char> mp, mp2;
-        for (int i=0; i<s.length(); ++i) {
-            if (mp[s[i]] && mp[s[i]]!=t[i]) return false;
-            if (mp2[t[i]] && mp2[t[i]]!=s[i]) return false;
-            mp[s[i]]=t[i];
-            mp2[t[i]]=s[i];
+        if (s.size() != t.size()) {
+            return false;
+        }
+        int hash[256] = {0};
+        bool isMapped[256] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            if (hash[s[i]] == 0 && !isMapped[t[i]]) {
+                hash[s[i]] = t[i];
+                isMapped[t[i]] = true;
+            }
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (hash[s[i]] != t[i]) {
+                return false;
+            }
         }
         return true;
     }
