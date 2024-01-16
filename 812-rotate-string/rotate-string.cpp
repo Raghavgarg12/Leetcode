a0ce90d22f44befdb0f7d55738689db389cf8f6a
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-01-16 14:20:19 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (88.00%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -5,12 +5,14 @@ public:
         char k;
         if(n!=goal.length()) return false;
         if(s==goal) return true;
-        for(i=0;i<n;i++){
-            k=s.at(0);
-            s.erase(0,1);
-            s=s+k;
-            if(s==goal) return true;
-        }
+        s+=s;
+        if(s.find(goal)<s.size()) return true;
+        // for(i=0;i<n;i++){
+        //     k=s.at(0);
+        //     s.erase(0,1);
+        //     s=s+k;
+        //     if(s==goal) return true;
+        // }
         return false;
     }
 };
