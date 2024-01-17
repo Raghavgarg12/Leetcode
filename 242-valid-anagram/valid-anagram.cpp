f1739104ba03680f2ffcc4bae4354c4187108009
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-01-17 12:23:10 +0530

    Time: 7 ms (73.99%) | Memory: 7.7 MB (79.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,33 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         unordered_map<char,int> map;
+//         int n=s.length(),i;
+//         if(n!=t.length()) return false;
+//         for(i=0;i<n;i++){
+//             map[s.at(i)]++;
+//         }
+//         for(i=0;i<n;i++){
+//             if(map[t.at(i)]<=0) return false;
+//             map[t.at(i)]--;
+//         }
+//         return true;
+//     }
+// };
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> map;
-        int n=s.length(),i;
-        if(n!=t.length()) return false;
-        for(i=0;i<n;i++){
-            map[s.at(i)]++;
+        if (s.length() != t.length()) {
+            return false;
         }
-        for(i=0;i<n;i++){
-            if(map[t.at(i)]<=0) return false;
-            map[t.at(i)]--;
+        int charCountS[26] = {0};
+        int charCountT[26] = {0};
+        for (char c : s) {
+            charCountS[c - 'a']++;
         }
-        return true;
+        for (char c : t) {
+            charCountT[c - 'a']++;
+        }
+        return memcmp(charCountS, charCountT, sizeof(charCountS)) == 0;
     }
 };
