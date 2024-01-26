b4044854228cbbb1e992ffd489c21a1aa767b635
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-01-26 12:15:56 +0530

    Time: 7 ms (70.30%) | Memory: 8.2 MB (42.45%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(string &a, string &b){
+        int n=a.length(),m=b.length();
+        if(m==0) return 0;
+        int x=ceil((double)m/n);
+        int i=x;
+        string s=a;
+        while(s.length()<m) s+=a;
+        while(s.find(b) == string::npos) {
+            if(i>x) return -1;
+            s+=a;
+            i++;
+        }
+        return i;
+    }
+    int repeatedStringMatch(string a, string b) {
+        return solve(a,b);  
+    }
+};
