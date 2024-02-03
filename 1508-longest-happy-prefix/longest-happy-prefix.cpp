9fdddd6046cc216fa3b895579560a3c3cce5af9c
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-03 08:55:28 +0530

    Time: 360 ms (5.02%) | Memory: 15.8 MB (92.98%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -3,11 +3,7 @@ public:
     string longestPrefix(string s) {
         int n = s.length();
         const string_view s1(s);
-        for(int i=n-1;i>=0;i--){
-            // string s1=s.substr(0,i), s2=s.substr(n-i,n);
-            if(s1.substr(0,i)==s1.substr(n-i,n)) return s.substr(0,i);
-            // if(s1==s2) return s1;
-        }
+        for(int i=n-1;i>=0;i--) if(s1.substr(0,i)==s1.substr(n-i,n)) return s.substr(0,i);
         return "";
     }
 };
