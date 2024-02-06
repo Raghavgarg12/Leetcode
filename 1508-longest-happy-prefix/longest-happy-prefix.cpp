2c09997aa0d7fb24c99916b0f31337f358a13744
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-06 18:42:28 +0530

    Time: 24 ms (98.63%) | Memory: 21.3 MB (36.14%) - LeetSync

--- 1508-longest-happy-prefix/longest-happy-prefix.cpp
+++ 1508-longest-happy-prefix/longest-happy-prefix.cpp
@@ -1,9 +1,32 @@
+// class Solution {
+// public:
+//     string longestPrefix(string s) {
+//         int n = s.length();
+//         const string_view s1(s);
+//         for(int i=n-1;i>=0;i--) if(s1.substr(0,i)==s1.substr(n-i,n)) return s.substr(0,i);
+//         return "";
+//     }
+// };
 class Solution {
 public:
     string longestPrefix(string s) {
-        int n = s.length();
-        const string_view s1(s);
-        for(int i=n-1;i>=0;i--) if(s1.substr(0,i)==s1.substr(n-i,n)) return s.substr(0,i);
-        return "";
+        string ans="";
+        vector<int>pos(s.size(), 0);
+        int i=0, j=1;
+        while(j<s.size()){
+            if(s[i]==s[j]){
+                pos[j]=i+1;
+                i++;
+                j++;
+            }
+            else if(i){
+                i=pos[i-1];
+            }
+            else{
+                j++;
+            }
+        }
+        ans=s.substr(0, i);
+        return ans;
     }
 };
