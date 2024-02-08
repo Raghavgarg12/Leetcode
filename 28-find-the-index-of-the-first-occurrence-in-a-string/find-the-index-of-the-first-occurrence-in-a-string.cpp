9d4aeebb8d30714da236217dd10dd6d8ad37335a
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-08 16:30:04 +0530

    Time: 3 ms (43.74%) | Memory: 7.5 MB (15.73%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,30 +1,51 @@
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//             // return haystack.find(needle);
+//         int n=haystack.length(),m=needle.length();
+//         vector<int>pos(m, 0);
+//         int i=0,j=1;
+//         while(j<m){
+//             if(needle[i]==needle[j]){
+//                 pos[j]=i+1;
+//                 i++;
+//                 j++;
+//             }
+//             else if(i) i=pos[i-1];
+//             else j++;
+//         }
+//         i=0;
+//         j=0;
+//         while(j<n && i<m){
+//             if(needle[i]==haystack[j]){
+//                 i++;
+//                 j++;
+//             }
+//             else if(i) i=pos[i-1];
+//             else j++;
+//             if (i == m) return j-m;
+//         }
+//         return (i == m) ? j-1 : -1;
+//     }
+// };
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-            // return haystack.find(needle);
-        int n=haystack.length(),m=needle.length();
-        vector<int>pos(m, 0);
-        int i=0,j=1;
-        while(j<m){
-            if(needle[i]==needle[j]){
-                pos[j]=i+1;
-                i++;
-                j++;
-            }
-            else if(i) i=pos[i-1];
-            else j++;
+    int strStr(string h, string n) {
+        int n1=h.size(),n2=n.size();
+        if(n2>n1){
+            return -1;
         }
-        i=0;
-        j=0;
-        while(j<n && i<m){
-            if(needle[i]==haystack[j]){
-                i++;
-                j++;
+        for(int i=0;i<=(n1-n2);i++){
+            if(h[i]==n[0]){
+                int j;
+                for(j=i;j<i+n2;j++){
+                    if(h[j]!=n[j-i]){
+                        break;
+                    }
+                }
+                if(j==i+n2){return i;}
             }
-            else if(i) i=pos[i-1];
-            else j++;
-            if (i == m) return j-m;
         }
-        return (i == m) ? j-1 : -1;
+        return -1;
     }
 };
