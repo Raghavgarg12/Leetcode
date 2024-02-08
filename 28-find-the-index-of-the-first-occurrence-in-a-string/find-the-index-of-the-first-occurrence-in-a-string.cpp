3c3fefb629da2ab3b091afbf558bf1306f7f20ba
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-08 15:58:55 +0530

    Time: 3 ms (43.74%) | Memory: 7.5 MB (15.73%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -23,38 +23,8 @@ public:
             }
             else if(i) i=pos[i-1];
             else j++;
-            cout<<i<<endl;
             if (i == m) return j-m;
         }
-        cout<<j<<endl;
         return (i == m) ? j-1 : -1;
-//         int i,j=0;
-//         for(i=0;i<n;i++){
-//             if(haystack[i]==needle[j]) j++;
-//             else j=0;
-//             if(j==m) return i-j+1;
-//         }
-//         return -1;
     }
 };
-// class Solution {
-// public:
-//     int strStr(string h, string n) {
-//         int n1=h.size(),n2=n.size();
-//         if(n2>n1){
-//             return -1;
-//         }
-//         for(int i=0;i<=(n1-n2);i++){
-//             if(h[i]==n[0]){
-//                 int j;
-//                 for(j=i;j<i+n2;j++){
-//                     if(h[j]!=n[j-i]){
-//                         break;
-//                     }
-//                 }
-//                 if(j==i+n2){return i;}
-//             }
-//         }
-//         return -1;
-//     }
-// };
