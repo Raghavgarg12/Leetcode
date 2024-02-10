cc3ba3147b8327f3ce86c3d07fd9cf38b9aeb008
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-10 22:39:46 +0530

    Time: 956 ms (5.00%) | Memory: 487.8 MB (5.20%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,25 +1,40 @@
 class Solution 
 {
 private:
-    string t;
-    int check(int l, int r, int ans = 0)
-    {
-        while(l >= 0 and r <= t.size())
-        {
-            if(t[l--] == t[r++]) ans++;
-            else break;
-        }
-        return ans;
+    // string t;
+    int pallindrome(string t){
+        string r=t;
+        reverse(t.begin(),t.end());
+        if(t==r) return 1;
+        return 0;
     }
-public:
-    int countSubstrings(string s) 
-    {
-        int n = size(s), ans = 0;t = s;
-        for(int i = 0; i < n; i++)
-        {
-            ans += check(i,i);    // odd length palindromes
-            ans += check(i,i+1); // even length palindromes
-        }
+    // int check(int l, int r, int ans = 0)
+    // {
+    //     while(l >= 0 and r <= t.size())
+    //     {
+    //         if(t[l--] == t[r++]) ans++;
+    //         else break;
+    //     }
+    //     return ans;
+    // }
+// public:
+//     int countSubstrings(string s) 
+//     {
+//         int n = size(s), ans = 0;t = s;
+//         for(int i = 0; i < n; i++)
+//         {
+//             ans += check(i,i);    // odd length palindromes
+//             ans += check(i,i+1); // even length palindromes
+//         }
+//         return ans;
+//     }
+// };
+    public:
+    int countSubstrings(string s){
+        int n =s.length(),ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++) ans+=pallindrome(s.substr(i,j-i+1));
+            }
         return ans;
     }
 };
