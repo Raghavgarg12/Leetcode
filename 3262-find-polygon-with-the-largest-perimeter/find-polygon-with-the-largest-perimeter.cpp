50ba4954e7ad2150df9da9f00cf0a568fcde8a28
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-15 17:19:04 +0530

    Time: 134 ms (88.72%) | Memory: 83.2 MB (36.11%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.cpp
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.cpp
@@ -5,15 +5,10 @@ public:
         if(n<3) return -1;
         sort(nums.begin(),nums.end());
         long long sum = accumulate(nums.begin(), nums.end()-1, 0LL);
-        cout<<sum<<endl;
         for(int i=n-1;i>=2;i--){
-            if(sum>nums[i]) {
-                cout<<"yes";
-                return sum+nums[i];
-            }
+            if(sum>nums[i]) return sum+nums[i];
             else sum-=nums[i-1];
         }
         return -1;
-        
     }
 };
