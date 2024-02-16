ff3e35d89e0382b22455eaf0373ffba476e7297d
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-16 21:13:10 +0530

    Time: 695 ms (5.10%) | Memory: 66.1 MB (60.95%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.cpp
@@ -6,15 +6,6 @@ public:
         vector<int>v;
         for(auto a :mp) v.push_back(a.second);
         sort(v.begin(),v.end());
-        // std::vector<std::pair<int, int>> fv(mp.begin(), mp.end());
-    // Sort the vector based on frequencies (second values) in descending order
-    // std::sort(fv.begin(), fv.end(), [](const auto& left, const auto& right) {
-    //     return left.second < right.second;
-    // });
-    // int n=v.size();
-    // for(int i=0;i<n;i++){
-    //     if(k>=)
-    // }
     while(k>0){
         k-=v[0];
         if(k>=0) v.erase(v.begin());
