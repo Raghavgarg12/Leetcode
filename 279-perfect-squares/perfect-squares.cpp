4edb841c93b5e959d2caa6423709a1da4a861402
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-16 10:24:19 +0530

    Time: 51 ms (51.08%) | Memory: 10.6 MB (45.52%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        // dp vector to store answer from [1, n]
+        vector<int> dp(n+1);
+
+        // Mark ans for perfect square numbers as 1
+        for(int i=1; i*i<=n; i++){
+            dp[i*i] = 1;
+        }
+
+        // find the answer from [1, n]
+        for(int i=1; i<=n; i++){
+            // if perfect square then skip
+            if(dp[i] == 1) continue;
+
+            // find the root of i
+            // current ans is min of ans[j*j] + ans[i - j*j]
+            int root = sqrt(i), mn = INT_MAX;
+            for(int j=root; j>=1; j--){
+                mn = min(mn, dp[j*j] + dp[i - j*j]);
+            }
+            dp[i] = mn;
+        }
+
+        return dp[n];
+    }
+};
