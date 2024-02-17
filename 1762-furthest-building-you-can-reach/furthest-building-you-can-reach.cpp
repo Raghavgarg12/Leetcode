7c490dd2effe4f1b29c003f0b0506fa37465546a
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-17 17:03:26 +0530

    Time: 85 ms (34.57%) | Memory: 57.6 MB (21.59%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.cpp
@@ -1,36 +1,50 @@
+// class Solution {
+// public:
+//     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+//         int n = heights.size();
+//         if(n<=ladders) return n-1;
+//         vector<int>h;
+//         int x;
+//         // vector to store difference of heights
+//         for(int i=1;i<n;i++){
+//             x=heights[i]-heights[i-1];
+//             if(x>0) h.push_back(x);
+//             else h.push_back(0);
+//         }
+//         int left = 0;
+//         int right = n - 1;
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             std::vector<int> h2(h.begin(), h.begin() + mid);
+//             std::sort(h2.begin(), h2.end());
+//             if (std::accumulate(h2.begin(), h2.begin() + std::max(0, mid - ladders), 0) > bricks) {
+//                 right = mid - 1;
+//             } else {
+//                 left = mid + 1;
+//             }
+//         }
+//         return left-1;
+//     }
+// };
 class Solution {
 public:
-    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-        int n = heights.size();
-        if(n<=ladders) return n-1;
-        vector<int>h;
-        int x;
-        // vector to store difference of heights
-        for(int i=1;i<n;i++){
-            x=heights[i]-heights[i-1];
-            if(x>0) h.push_back(x);
-            else h.push_back(0);
-        }
-        // while(true){
-        //     vector<int>h2(h);
-        //     sort(h2.begin(),h2.end());
-        //     if(accumulate(h2.begin(),h2.begin()+h2.size()-ladders,0LL)>bricks) h.pop_back();
-        //     else return h2.size();
-        // }
-        // return 0;
-        int left = 0;
-        int right = n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            std::vector<int> h2(h.begin(), h.begin() + mid);
-            std::sort(h2.begin(), h2.end());
-            if (std::accumulate(h2.begin(), h2.begin() + std::max(0, mid - ladders), 0) > bricks) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+    int furthestBuilding(vector<int>& h, int b, int l) {
+        // Priority Queue for storing the bricks used in each step in decreasing order (Max at top)
+        priority_queue<int> p;
+        int i=0, diff =0; 
+        for(i=0; i<h.size()-1; i++){ 
+            diff = h[i+1]-h[i];
+            if(diff <= 0) continue;
+            b -= diff; 
+            p.push(diff); 
+            if(b < 0){
+                b += p.top(); 
+                p.pop(); 
+                l--;
             }
+            if(l < 0) break;
         }
-
-        return left-1;
+        return i;
     }
 };
+
