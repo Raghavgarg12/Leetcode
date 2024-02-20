3a78504792ae0beb63174be6df99a5f377371a8e
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-20 12:28:24 +0530

    Time: 11 ms (85.38%) | Memory: 20.4 MB (32.85%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,7 +2,6 @@ using namespace std;
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         int n=nums.size();
         int s=accumulate(nums.begin(),nums.end(),0);
         int sum=(n*(n+1))/2;
