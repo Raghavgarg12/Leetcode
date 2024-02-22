820d8814aa163a86e81072c8b15cc8227294b438
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-22 21:56:16 +0530

    Time: 145 ms (50.15%) | Memory: 72.3 MB (26.18%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,21 +1,35 @@
+// class Solution {
+// public:
+//     int findJudge(int n, vector<vector<int>>& trust) {
+//         int l=trust.size();
+//         // if(l==0){
+//         //     if(n<2) return n;
+//         //     else return -1;
+//         // }
+//         unordered_map<int,int>mp;
+//         int ans=0;
+//         for(int i=0;i<l;i++){
+//             mp[trust[i][0]]--;
+//             mp[trust[i][1]]++;
+//             if(mp[trust[i][1]]>mp[ans]) ans=trust[i][1];
+//         }
+//         cout<<mp[ans];
+//         if(mp[ans]==n-1) return ans;
+//         return -1;
+//     }
+// };
+
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        int l=trust.size();
-        if(l==0){
-            if(n<2) return n;
-            else return -1;
-        }
-        unordered_map<int,int>mp;
-        int ans=0;
-        for(int i=0;i<l;i++){
-            mp[trust[i][0]]--;
-            mp[trust[i][1]]++;
-            if(mp[trust[i][1]]>mp[ans]) ans=trust[i][1];
-            cout<<ans<<endl;
-        }
-        cout<<ans;
-        if(mp[ans]<n-1) return -1;
-        return ans;
+    int findJudge(int N, vector<vector<int>>& trust) {
+		vector<int> in(N + 1), out(N + 1);
+		for (auto a : trust) {
+			++out[a[0]];
+			++in[a[1]];
+		}
+		for (int i = 1; i <= N; ++i) {
+			if (in[i] == N - 1 && out[i] == 0) return i;
+		}
+		return -1;
     }
 };
