a6806b93c0522333d3b2868d68cd99a54ded227f
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-02-29 17:39:00 +0530

    Time: 186 ms (89.99%) | Memory: 149.2 MB (76.25%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -12,49 +12,35 @@
 class Solution {
 public:
     bool isEvenOddTree(TreeNode* root) {
-        // Create a queue for level-order traversal
-        queue<TreeNode*> q;
-        // Push the root node into the queue
-        q.push(root);
-
+        queue<TreeNode*> q; // Create a queue for level-order traversal
+        q.push(root); // Push the root node into the queue
         // Initialize variables for iteration and flag to alternate between even and odd levels
         int i;
         int flag = 0;
-
         // Continue the traversal until the queue is empty
         while (!q.empty()) {
             // Get the number of nodes at the current level
             int len = q.size();
             // Initialize prev with extreme values based on the flag
             int prev = flag ? INT_MAX : INT_MIN;
-
             // Iterate through nodes at the current level
             for (i = 0; i < len; i++) {
-                // Dequeue a node
                 TreeNode* node = q.front();
-                q.pop();
-
-                // Check conditions based on even or odd level
-                if (flag) {
+                q.pop(); // Dequeue a node
+                if (flag) { // Check conditions based on even or odd level
                     // For even level: value should be odd and strictly decreasing
                     if (node->val % 2 != 0 || node->val >= prev) return false;
-                } else {
-                    // For odd level: value should be even and strictly increasing
-                    if (node->val % 2 == 0 || node->val <= prev) return false;
                 }
-
+                // For odd level: value should be even and strictly increasing
+                else if (node->val % 2 == 0 || node->val <= prev) return false;
                 // Enqueue left and right children if present
                 if (node->left) q.push(node->left);
                 if (node->right) q.push(node->right);
-
                 // Update prev with the current node's value
                 prev = node->val;
             }
-
-            // Toggle the flag for the next level
-            flag = !flag;
+            flag = !flag; // Toggle the flag for the next level
         }
-
         // If the traversal completes without any issues, return true
         return true;
     }
