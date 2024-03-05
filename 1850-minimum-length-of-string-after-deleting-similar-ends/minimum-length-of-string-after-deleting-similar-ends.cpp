1d9e8cf1052f8488394ee53d205b214f4f18002f
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-03-05 16:03:39 +0530

    Time: 23 ms (80.00%) | Memory: 13.8 MB (81.03%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        int l=0,r=s.length()-1;
-        while(s[l]==s[r] && l<r){
-            while(s[l]==s[l+1] && l+1<r) l++;
-            while(s[r]==s[r-1] && l+1<r) r--;
-            if(l<r){
+        // Initialize two pointers, 'l' pointing to the start of the string and 'r' pointing to the end.
+        int l = 0, r = s.length() - 1;
+        // Continue the loop as long as characters at 'l' and 'r' are equal and 'l' is less than 'r'.
+        while (s[l] == s[r] && l < r) {
+            // Move 'l' to the right until a different character is encountered or 'l+1' reaches 'r'.
+            while (s[l] == s[l + 1] && l + 1 < r)
+                l++;
+            // Move 'r' to the left until a different character is encountered or 'r-1' reaches 'l'.
+            while (s[r] == s[r - 1] && l + 1 < r)
+                r--;
+            // If there are more characters between 'l' and 'r', move both pointers inward.
+            if (l < r) {
                 l++;
                 r--;
             }
-            cout<<l<<r<<endl;
         }
-        return r-l+1;
+        // Return the length of the remaining substring between 'l' and 'r'.
+        return r - l + 1;
     }
 };
