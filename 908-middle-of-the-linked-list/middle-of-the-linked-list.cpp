3d23d9a061ff7b119ff841548f8a8a85e53df363
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-03-07 01:02:17 +0530

    Time: 4 ms (29.84%) | Memory: 8.8 MB (7.67%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -11,11 +11,13 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
+        // Initialize two pointers, n1 and n2, both starting at the head of the linked list
         ListNode* n1 = head;
         ListNode* n2 = head;
+        // Traverse the linked list using two pointers
         while(n2!=NULL && n2->next!=NULL){
-            n1=n1->next;
-            n2=n2->next->next;
+            n1=n1->next; // Move n1 one step at a time
+            n2=n2->next->next; // Move n2 two steps at a time
         }
         return n1;
     }
