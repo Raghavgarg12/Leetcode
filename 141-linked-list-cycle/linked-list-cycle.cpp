6088e99dd2a254ac14999dd4c3be5ab4fab0efd6
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-03-06 16:24:03 +0530

    Time: 7 ms (85.78%) | Memory: 10.6 MB (53.92%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,15 +9,23 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL) return false;
-        // Create an unordered set to keep track of visited nodes
-        unordered_set<ListNode*>mp;
-        // Traverse the linked list
-        while(head!=NULL){
-            if(mp.count(head)>0) return true; // Check if the current node has been visited before
-            mp.insert(head); // Insert the current node into the set
-            head=head->next; // Move to the next node in the linked list
+        // if(head==NULL) return false;
+        // // Create an unordered set to keep track of visited nodes
+        // unordered_set<ListNode*>mp;
+        // // Traverse the linked list
+        // while(head!=NULL){
+        //     if(mp.count(head)>0) return true; // Check if the current node has been visited before
+        //     mp.insert(head); // Insert the current node into the set
+        //     head=head->next; // Move to the next node in the linked list
+        // }
+        // return false; // No cycle
+        ListNode* p1 = head; // Initialize slow pointer (tortoise) to the head of the linked list
+        ListNode* p2 = head; // Initialize fast pointer (hare) to the head of the linked list
+        while(p2!=NULL && p2->next!=NULL){ // Traverse the linked list using two pointers
+            p1=p1->next; // Move slow pointer one step at a time
+            p2=p2->next->next; // Move fast pointer two steps at a time
+            if(p1==p2) return true; // Check if the pointers meet, indicating the presence of a cycle
         }
-        return false; // No cycle
+        return false; // If the loop completes without meeting, there is no cycle
     }
 };
