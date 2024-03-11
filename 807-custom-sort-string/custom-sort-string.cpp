899643c976e3ae65097d9c3ff1d2030a2f8e16ba
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-03-11 22:22:26 +0530

    Time: 3 ms (50.27%) | Memory: 8.3 MB (30.10%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     string customSortString(string order, string s) {
         unordered_map<char,int>mp;
-        int n=s.length();
-        int i;
         string ans="";
-        for(i=0;i<n;i++) mp[s[i]]++;
-        n=order.length();
-        cout<<mp['t'];
-        for(i=0;i<n;i++){
-            ans.append(mp[order[i]],order[i]);
-            mp[order[i]]=0;
+        for(const char& x:s) mp[x]++;
+        for(const char& x:order){
+            ans.append(mp[x],x);
+            mp[x]=0;
         }
-        for(char x: s){
+        for(const char& x:s){
             if(mp[x]!=0) {
                 ans.append(mp[x],x);
                 mp[x]=0;
