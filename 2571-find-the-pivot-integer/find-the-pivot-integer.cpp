4154ff09f66832bb51683675c7e4f3459e35f056
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-03-13 17:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (73.82%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int pivotInteger(int n) {
         int a = (n*(n+1))/2;
-        for(int i=0;i<=n;i++){
-            int b = (i*(i+1))/2;
+        int b=0;
+        for(int i=1;i<=n;i++){
+            b+=i;
             if(a-b+i==b) return i; 
         }
         return -1;
