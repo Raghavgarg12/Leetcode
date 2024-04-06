5341dbaf2aeddd399831eab81e155042e2ba90c7
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-04-06 23:44:18 +0530

    Time: 24 ms (37.61%) | Memory: 13.1 MB (46.42%) - LeetSync

---
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int leftCount = 0;
+        int rightCount = 0;
+        std::stack<char> stack;
+
+        // Pass 1
+        for (char ch : s) {
+            if (ch == '(') {
+                leftCount++;
+            } else if (ch == ')') {
+                rightCount++;
+            }
+            if (rightCount > leftCount) {
+                rightCount--;
+                continue;
+            } else {
+                stack.push(ch);
+            }
+        }
+        std::string result = "";
+        
+        // Pass 2
+        while (!stack.empty()) {
+            char currentChar = stack.top();
+            stack.pop();
+            if (leftCount > rightCount && currentChar == '(') {
+                leftCount--;
+            } else {
+                result += currentChar;
+            }
+        }
+        reverse(result.begin(),result.end());
+        return result;
+    }
+};
