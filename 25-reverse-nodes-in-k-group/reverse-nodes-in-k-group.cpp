0865300d1dc676e42157e7fedfbcb35c6956fe65
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-05-09 23:14:17 +0530

    Time: 7 ms (86.56%) | Memory: 15.2 MB (10.42%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -53,59 +53,5 @@ public:
         }
         prev->next=start;
         return ans;
-        // vector<int>ans(k);
-        // ListNode* answer = new ListNode();
-        // ListNode* iter = answer;
-        // int n=0;
-        // while(temp){
-        //     ans[n]=temp->val;
-        //     n++;
-        //     temp=temp->next;
-        //     if(n==k){
-        //         while(n){
-        //             // cout<<ans[n-1]<<endl;
-        //             iter->val=ans[n-1];
-        //             iter->next=new ListNode();
-        //             iter=iter->next;
-        //             n--;
-        //         }
-        //     }
-        // }
-        // cout<<n<<endl;
-        // if(n>0){
-        //     while(n){
-        //         cout<<ans[n-1];
-        //         iter->val=ans[n-1];
-        //         // iter->next = new ListNode()
-        //         iter=iter->next;
-        //         n--;
-        //     }
-        // }
-        // else delete iter;
-        // return answer;
-        // ListNode* temp = head;
-        // vector<int>ans;
-        // int i=0,n=1;
-        // while(temp){
-        //     for(i=n*k;i>n*k-k;i--){
-        //         cout<<temp->val;
-        //         cout<<i<<endl;
-        //         ans.insert(ans.begin()+i,temp->val);
-        //         temp=temp->next;
-        //         if(!temp) break;
-        //     }
-        //     n++;
-        // }
-        // i=0;
-        // ListNode* answer = new ListNode();
-        // ListNode* iter = answer;
-        // while(k-1){
-        //     iter->val=ans[i++];
-        //     iter->next = new ListNode();
-        //     iter=iter->next;
-        // }
-        // iter->val=ans[i];
-        // return answer;
-        // ListNode* temp = head;
     }
 };
