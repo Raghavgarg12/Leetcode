4f168292eeb1edeaf94e20645e7da04a7519c871
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-05-16 09:33:53 +0530

    Time: 17 ms (6.24%) | Memory: 17.3 MB (64.84%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -11,12 +11,15 @@
  */
 class Solution {
 public:
-    int check(TreeNode* root){
-        if(root->val==1 || root->val==0) return root->val;
-        else if(root->val==2) return(check(root->left) || check(root->right));
-        else return(check(root->left) && check(root->right));
-    }
+    // int check(TreeNode* root){
+    //     if(root->val==1 || root->val==0) return root->val;
+    //     else if(root->val==2) return(check(root->left) || check(root->right));
+    //     else return(check(root->left) && check(root->right));
+    // }
     bool evaluateTree(TreeNode* root) {
-        return check(root);
+        // return check(root);
+        if(root->val==1 || root->val==0) return root->val;
+        else if(root->val==2) return(evaluateTree(root->left) || evaluateTree(root->right));
+        else return(evaluateTree(root->left) && evaluateTree(root->right));
     }
 };
