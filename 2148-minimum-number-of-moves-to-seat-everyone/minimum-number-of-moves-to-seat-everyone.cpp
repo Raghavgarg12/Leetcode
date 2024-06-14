32af75edbb04fcc1065658513d4884aa2910a9d1
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-06-14 19:40:46 +0530

    Time: 4 ms (74.67%) | Memory: 21 MB (51.98%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,10 +1,38 @@
+// class Solution {
+// public:
+//     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+//         sort(seats.begin(),seats.end());
+//         sort(students.begin(),students.end());
+//         int ans=0;
+//         for(int i=0;i<seats.size();i++) ans+=abs(seats[i]-students[i]);
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(seats.begin(),seats.end());
-        sort(students.begin(),students.end());
-        int ans=0;
-        for(int i=0;i<seats.size();i++) ans+=abs(seats[i]-students[i]);
-        return ans;
+        int maxPosition = max(*max_element(seats.begin(),seats.end()), *max_element(students.begin(),students.end()));
+        vector<int> differences(maxPosition, 0);
+        for (int position : seats) differences[position - 1]++;
+        for (int position : students) differences[position - 1]--;
+
+        int moves = 0;
+        int unmatched = 0;
+        for (int difference : differences) {
+            moves += abs(unmatched);
+            unmatched += difference;
+        }
+        return moves;
+    }
+
+private:
+    int findMax(const vector<int>& array) {
+        int maximum = 0;
+        for (int num : array) {
+            if (num > maximum) {
+                maximum = num;
+            }
+        }
+        return maximum;
     }
 };
