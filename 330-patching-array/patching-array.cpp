8f4f7360c999e6ae23b0926e071bdcba983b3041
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-06-17 02:29:15 +0530

    Time: 9 ms (21.55%) | Memory: 13.8 MB (45.23%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss=1;
+        int ans=0;
+        int i=0;
+        while(miss<=n){
+            if(i<nums.size() && nums[i]<=miss){
+                miss+=nums[i];
+                i++;
+            }
+            else{
+                miss+=miss;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
