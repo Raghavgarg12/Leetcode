13767c3826c15e9550898a3bdc834f9e610e6781
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-06-23 02:56:28 +0530

    Time: 5 ms (11.86%) | Memory: 8.8 MB (23.13%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
 vector<vector<int>>ans;
-    void check(vector<int>temp,int start, int k, int n, vector<int>nums){
+    void check(vector<int>temp,int start, int k, int n){
         if(k==0 && n==0) {
             ans.push_back(temp);
             return;
         }
-        for(int i=start;i<nums.size() && n>0;i++){
-            temp.push_back(nums[i]);
-            check(temp,i+1,k-1,n-nums[i],nums);
+        for(int i=start;i<=9 && n>0;i++){
+            temp.push_back(i);
+            check(temp,i+1,k-1,n-i);
             temp.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int>nums={1,2,3,4,5,6,7,8,9};
         vector<int>temp;
-        check(temp,0,k,n,nums);
+        check(temp,1,k,n);
         return ans;
     }
 };
