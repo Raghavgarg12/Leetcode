052946eb71f782c6a597975d6e42bee6ca5fee15
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-06-25 22:13:26 +0530

    Time: 5 ms (15.49%) | Memory: 10.1 MB (42.71%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -1,41 +1,15 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-vector<int>nums;
 int sum=0;
     void dfs(TreeNode* &root){
         if(root==NULL) return;
         dfs(root->right);
         root->val+=sum;
         sum=root->val;
-        dfs(root->left);
-        // nums.push_back(root->val);
-        
+        dfs(root->left);   
     }
-    // void assign(TreeNode* root, int a){
-    //     if(root->NULL) return;
-    //     dfs(root->left,a--)
-
-    // }
     TreeNode* bstToGst(TreeNode* root) {
         dfs(root);
-        // int temp=0;
-        // for(int i=nums.size()-1;i>=0;i--) {
-        //     temp+=nums[i];
-        //     nums[i]=temp;
-        //     cout<<temp<<endl;
-        // }
-        // assign(root,nums.size());
         return root;
     }
 };
