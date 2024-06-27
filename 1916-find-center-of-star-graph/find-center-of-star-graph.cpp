f16a2074d3d1c825f5c8187307996600122acc95
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-06-27 18:35:00 +0530

    Time: 117 ms (96.18%) | Memory: 70.9 MB (54.65%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size();
-        int m=edges[0].size();
-        int maxi=0;
-        vector<int>arr(n+2,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                arr[edges[i][j]]++;
-                // cout<<arr[edges[i][j]-1]<<endl;
-                maxi=max(maxi,arr[edges[i][j]]);
-            }
-        }
-        // cout<<arr[0];
-        int ans=find(arr.begin(),arr.end(),maxi)-arr.begin();
-        return ans;
+        // int n = edges.size();
+        // int m=edges[0].size();
+        // int maxi=0;
+        // vector<int>arr(n+2,0);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         arr[edges[i][j]]++;
+        //         maxi=max(maxi,arr[edges[i][j]]);
+        //     }
+        // }
+        // int ans=find(arr.begin(),arr.end(),maxi)-arr.begin();
+        // return ans;
+        vector<int> firstEdge = edges[0];
+        vector<int> secondEdge = edges[1];
+        return (firstEdge[0] == secondEdge[0] || firstEdge[0] == secondEdge[1])
+                   ? firstEdge[0] : firstEdge[1];
     }
 };
