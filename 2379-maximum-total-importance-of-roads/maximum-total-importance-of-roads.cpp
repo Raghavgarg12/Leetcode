afc36328014b305762806fa108f7dbcac637f0ed
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-06-29 03:21:37 +0530

    Time: 320 ms (45.61%) | Memory: 136.1 MB (44.26%) - LeetSync

---
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool comp(const pair<int,int>& a, const pair<int,int>& b) {
+    return a.second < b.second;
+}
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<long long>nums(n,0);
+        vector<pair<int,int>>arr(n);
+        long long ans=0;
+        for(auto road: roads){
+            nums[road[0]]++;
+            nums[road[1]]++;
+        }
+        for(int i=0;i<n;i++){
+            arr[i]={i,nums[i]};
+        }
+        sort(arr.begin(),arr.end(),comp);
+        for(int i=0;i<n;i++){
+            arr[i].second=i+1;
+        }
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<n;i++){
+            ans+=nums[i]*arr[i].second;
+        }
+        return ans;
+    }
+};
