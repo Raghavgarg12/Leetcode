f7af71b791ea473501ebe945736432feca0c8ebd
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-07-10 01:50:18 +0530

    Time: 137 ms (89.80%) | Memory: 76.3 MB (95.63%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         double time=customers[0][0],wait=0;
-        for(auto x: customers){
+        for(const auto& x: customers){
             time=max<double>(time,x[0])+x[1];
             wait+=time-x[0];
         }
