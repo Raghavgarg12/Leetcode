6d2cba691d31732b611a09c8a15795f1284e5b6b
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-07-10 01:49:27 +0530

    Time: 178 ms (23.03%) | Memory: 86.6 MB (7.29%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -3,9 +3,8 @@ public:
     double averageWaitingTime(vector<vector<int>>& customers) {
         double time=customers[0][0],wait=0;
         for(auto x: customers){
-            cout<<wait<<" "<<time<<endl;
-            wait+=max(time-x[0],0.00)+x[1];
             time=max<double>(time,x[0])+x[1];
+            wait+=time-x[0];
         }
         return wait/customers.size();
     }
