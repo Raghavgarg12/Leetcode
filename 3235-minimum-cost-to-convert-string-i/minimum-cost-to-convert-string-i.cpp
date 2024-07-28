f647424f0347bc8a5031e7420285f6058bab7b57
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-07-28 22:57:46 +0530

    Time: 175 ms (62.74%) | Memory: 99 MB (54.52%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,35 +1,23 @@
 class Solution {
 public:
-    long long minimumCost(string source, string target, vector<char>& original,
-                          vector<char>& changed, vector<int>& cost) {
-        long long totalCost = 0;
-        vector<vector<long long>> minCost(26, vector<long long>(26, INT_MAX));
-        for (int i = 0; i < original.size(); ++i) {
-            int startChar = original[i] - 'a';
-            int endChar = changed[i] - 'a';
-            minCost[startChar][endChar] =
-                min(minCost[startChar][endChar], (long long)cost[i]);
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+        vector<vector<long long>>mincost(26,vector<long long>(26,INT_MAX));
+        long long total=0,n=original.size(),m=source.size();
+        for(int i=0;i<n;i++){
+            mincost[original[i]-'a'][changed[i]-'a']=min((long long)cost[i],mincost[original[i]-'a'][changed[i]-'a']);
         }
-        for (int k = 0; k < 26; ++k) {
-            for (int i = 0; i < 26; ++i) {
-                for (int j = 0; j < 26; ++j) {
-                    minCost[i][j] =
-                        min(minCost[i][j], minCost[i][k] + minCost[k][j]);
+        for(int k=0;k<26;k++){
+            for(int i=0;i<26;i++){
+                for(int j=0;j<26;j++){
+                    mincost[i][j]=min(mincost[i][j],mincost[i][k]+mincost[k][j]);
                 }
             }
         }
-        for (int i = 0; i < source.size(); ++i) {
-            if (source[i] == target[i]) {
-                continue;
-            }
-            int sourceChar = source[i] - 'a';
-            int targetChar = target[i] - 'a';
-            if (minCost[sourceChar][targetChar] >= INT_MAX) {
-                return -1;
-            }
-            totalCost += minCost[sourceChar][targetChar];
+        for(int i=0;i<m;i++){
+            if(source[i]==target[i]) continue;
+            if(mincost[source[i]-'a'][target[i]-'a']>=INT_MAX) return -1;
+            total+=mincost[source[i]-'a'][target[i]-'a'];
         }
-
-        return totalCost;
+        return total;
     }
 };
