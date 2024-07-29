dfce705e1111d362b3df9218aa7ae6bd5cb99def
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-07-29 19:31:03 +0530

    Time: 450 ms (76.23%) | Memory: 206.6 MB (32.96%) - LeetSync

--- 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
-        vector<vector<int>> adj(n + 1);
-        // Build the adjacency list.
-        for (auto& edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        vector<vector<int>>adjlist(n+1);
+        for(auto x:edges){
+            adjlist[x[0]].push_back(x[1]);
+            adjlist[x[1]].push_back(x[0]);
         }
-
-        queue<pair<int, int>> q;
-        vector<int> dist1(n + 1, -1), dist2(n + 1, -1);
-        // Start with node 1, with its minimum distance.
-        q.push({1, 1});
-        dist1[1] = 0;
-
-        while (!q.empty()) {
-            auto [node, freq] = q.front();
+        queue<pair<int,int>>q;
+        vector<int>dist1(n+1,-1),dist2(n+1,-1);
+        q.push({1,1});
+        dist1[1]=0;
+        while(!q.empty()){
+            auto [node,freq]=q.front();
             q.pop();
-
-            int timeTaken = freq == 1 ? dist1[node] : dist2[node];
-            // If the timeTaken falls under the red bracket, wait till the path turns green.
-            if ((timeTaken / change) % 2) {
-                timeTaken = change * (timeTaken / change + 1) + time;
-            } else {
-                timeTaken = timeTaken + time;
+            int taken=freq==1?dist1[node]:dist2[node];
+            if((taken/change)%2==1){ //red
+                taken+=(change-(taken)%change)+time;
             }
-
-            for (auto& neighbor : adj[node]) {
-                if (dist1[neighbor] == -1) {
-                    dist1[neighbor] = timeTaken;
-                    q.push({neighbor, 1});
-                } else if (dist2[neighbor] == -1 && dist1[neighbor] != timeTaken) {
-                    if (neighbor == n) return timeTaken;
-                    dist2[neighbor] = timeTaken;
-                    q.push({neighbor, 2});
+            else taken+=time;
+            for(int neigh: adjlist[node]){
+                if(dist1[neigh]==-1){
+                    dist1[neigh]=taken;
+                    q.push({neigh,1});
+                }
+                else if(dist2[neigh]==-1 && dist1[neigh]!=taken){
+                    if(neigh==n) return taken;
+                    dist2[neigh]=taken;
+                    q.push({neigh,2});
                 }
             }
         }
