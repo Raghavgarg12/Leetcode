1cc9f1b72e4212fa148fb272dbfca305e28d3ecc
Raghavgarg12 <97753861+Raghavgarg12@users.noreply.github.com>
2024-08-02 00:04:09 +0530

    Time: 4 ms (82.75%) | Memory: 17.7 MB (16.71%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -3,8 +3,11 @@ public:
     int countSeniors(vector<string>& details) {
         int n=details.size(),ans=0;
         for(string s : details){
-            int age=stoi(s.substr(11,2));
-            if(age>60) ans++;
+            int age=s[11]-'0';
+            if(age>6) ans++;
+            else if(age==6){
+                if(s[12]-'0'>0) ans++;
+            }
         }
         return ans;
     }
